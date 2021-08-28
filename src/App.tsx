c04c6ee7ee222bb3012a67f20c7c71bb05c66067
emykumba <emy@meetkumba.com>
2021-08-28 03:33:10 +0100

    added sticky component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./assets/css/bootstrap.min.css";
 import "./App.css";
 import AuthCard from "./components/AuthCard/AuthCard";
 import Routes from "./Routes";
+import Sticky from "react-sticky-el";
 
 function App() {
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
         <div className="col-sm-12 col-lg-4 mt-4">
-          <AuthCard />
+          <Sticky>
+            <AuthCard />
+          </Sticky>
         </div>
         <div className="col-sm-12 col-lg-12 mt-4">
           <Routes />
